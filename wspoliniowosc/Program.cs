c1569760c67d975940a2b9ab5d6b62106b2799b9
bak83 <bak.dariusz@gmail.com>
2020-01-19 17:22:55 +0100

    ready

--- wspoliniowosc/Program.cs
+++ wspoliniowosc/Program.cs
@@ -6,7 +6,24 @@ namespace wspoliniowosc
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] x = new int[3];
+			int[] y = new int[3];
+			int det;
+			for (int i = 1; i < 4; i++)
+			{
+				Console.WriteLine("Podaj współrzędne puntktu: " + i);
+				x[i - 1] = int.Parse(Console.ReadLine());
+				y[i - 1] = int.Parse(Console.ReadLine());
+			}
+			det = x[0] * y[1] + x[1] * y[2] + x[2] * y[0] - x[2] * y[1] - x[0] * y[2] - x[1] * y[0];
+			if (det == 0)
+			{
+				Console.WriteLine("punkty na płaszczyźnie są współliniowe");
+			}
+			else
+			{
+				Console.WriteLine("punkty na płaszczyźnie nie są współliniowe");
+			}
 		}
 	}
 }
